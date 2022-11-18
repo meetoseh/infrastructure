501350336f62d1280f09dadb4bd6adeb94a0193c
Timothy Moore <mtimothy984@gmail.com>
2022-11-18 05:26:41 -0800

    Add journey jwt secret env var

--- __main__.py
+++ __main__.py
@@ -38,6 +38,7 @@ apple_key_file = config.require("apple_key_file")
 image_file_jwt_secret = config.require_secret("image_file_jwt_secret")
 file_upload_jwt_secret = config.require_secret("file_upload_jwt_secret")
 content_file_jwt_secret = config.require_secret("content_file_jwt_secret")
+journey_jwt_secret = config.require_secret("journey_jwt_secret")
 
 # it's easy to misuse development_expo_urls, so we make sure it's valid
 for idx, url_str in enumerate(development_expo_urls):
@@ -86,6 +87,7 @@ def make_standard_webapp_configuration(args) -> str:
     image_file_jwt_secret: str = remaining[10]
     file_upload_jwt_secret: str = remaining[11]
     content_file_jwt_secret: str = remaining[12]
+    journey_jwt_secret: str = remaining[13]
 
     joined_rqlite_ips = ",".join(rqlite_ips)
     joined_redis_ips = ",".join(redis_ips)
@@ -109,6 +111,7 @@ def make_standard_webapp_configuration(args) -> str:
             f'export OSEH_IMAGE_FILE_JWT_SECRET="{image_file_jwt_secret}"',
             f'export OSEH_FILE_UPLOAD_JWT_SECRET="{file_upload_jwt_secret}"',
             f'export OSEH_CONTENT_FILE_JWT_SECRET="{content_file_jwt_secret}"',
+            f'export OSEH_JOURNEY_JWT_SECRET="{journey_jwt_secret}"',
             f"export ENVIRONMENT=production",
             f"export AWS_DEFAULT_REGION=us-west-2",
         ]
@@ -199,6 +202,7 @@ standard_configuration = pulumi.Output.all(
     image_file_jwt_secret,
     file_upload_jwt_secret,
     content_file_jwt_secret,
+    journey_jwt_secret,
 ).apply(make_standard_webapp_configuration)
 
 backend_rest.perform_remote_executions(standard_configuration)
